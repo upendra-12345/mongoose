bccbd350c5ebc74027a86ebac85a59c2b799c829
Upendra yadav <uy3234@gmail.com>
2026-09-10 21:53:37 +0530

    application level middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,7 @@
 const express= require("express");
 const app= express();
 
-
+// Application -level middleware
 app.use(m3);
 app.use(m1);
 
@@ -9,24 +9,31 @@ app.get("/",(req,res,next)=>{
     res.send("hii i am root server");
    
 });
-app.use(m2);
-app.get("/about",(req,res,next)=>{
+// app.use(m2);
+app.get("/about",m2,(req,res,next)=>{
     res.send("hello world");
     
 });
 
 
 function m3(req,res,next){
-     console.log("middleware 3 run");
-     next();
+    console.log(req.userid);
+    console.log("middleware 3 run");
+    next();
     
 }
 function m1(req,res,next){
     console.log("middleware 1 run");
+    req.userid=1;
     next();
 }
 function m2(req,res,next){
+    console.log("m2 ke ander", req.userid);
+    if(req.userid){
+       return  next();
+    }
     console.log("middleware 2 run");
+    res.send("not authorised");
     next();
 
 }
